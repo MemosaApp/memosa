e5c8ede2b695fc1f0872687fbbe074ac48d9a829
Ivan Montiel <idmontie@asu.edu>
2017-02-18 12:45:43 -0700

    Try using a hash

--- .deploy/install-meteor.sh
+++ .deploy/install-meteor.sh
@@ -3,7 +3,8 @@
 if ! [ -x "$(command -v meteor)" ]; then
   git clone git://github.com/meteor/meteor.git ~/meteor
   cd ~/meteor
-  git checkout tags/release/METEOR@1.4.2.3
+  # git checkout tags/release/METEOR@1.4.2.3
+  git checkout 8d76fb01ff4f3cb251609e20003ef793c64de4c8
   git submodule foreach 'git fetch && git fetch --tags'
   git submodule update --init --recursive
   cd -
