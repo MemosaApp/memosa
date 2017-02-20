5b911c28c5a111a1901a3534f852d5f946d1dee1
Ivan Montiel <idmontie@asu.edu>
2017-02-19 18:35:14 -0700

    Try using build essential

--- .deploy/install-meteor.sh
+++ .deploy/install-meteor.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# TODO cache meteor properly
+#
+# Example:
+#
+# if [ -d ~/.meteor ]; then sudo ln -s ~/.meteor/meteor /usr/local/bin/meteor; fi
+# if [ ! -e $HOME/.meteor/meteor ]; then curl https://install.meteor.com | sh; fi
+#
+
 if ! [ -x "$(command -v meteor)" ]; then
   git clone git://github.com/meteor/meteor.git ~/meteor
   cd ~/meteor
